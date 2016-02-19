e0936be9870245a6ac8165c04b6f70ee0196659b
naverstay <3a4f044785G->
2016-02-19 23:04:23 +0200

    large graph

--- js/dashboard_script.js
+++ js/dashboard_script.js
@@ -27,22 +27,113 @@ $(function ($) {
 
      }, 500);*/
 
+    $('.datePicker').datepicker({
+        beforeShowYear: function (date) {
+            if (date.getFullYear() == 2007) {
+                return false;
+            }
+        }
+    });
 
     $('body')
         .delegate('.bootstrap-select.filterSelect', 'hide.bs.dropdown', function () {
             $(this).closest('.hover-select-box').removeClass('opened');
         })
         .delegate('.bootstrap-select.filterSelect', 'click', function () {
-            console.log('open', this);
             $(this).closest('.hover-select-box').addClass('opened');
         });
+
+    $('.graphFilterDate').on('change', function () {
+        var firedEl = $(this);
+
+        console.log(firedEl.val());
+
+        return false;
+    });
 
 
 });
 
 function init_charts() {
 
+    var big_chart = [
+        {
+            "name": "Revenue",
+            "color": "#86f3b7",
+            "value": "35,489$",
+            "diff": "+8%",
+            "data": [
+                {"date": "9-Apr-12", "close": 180},
+                {"date": "7-Apr-12", "close": 221},
+                {"date": "5-Apr-12", "close": 400},
+                {"date": "4-Apr-12", "close": 320},
+                {"date": "3-Apr-12", "close": 229},
+                {"date": "2-Apr-12", "close": 150}
+            ]
+        },
+        {
+            "name": "Orders",
+            "color": "#4C89FF",
+            "value": "490",
+            "diff": "-9%",
+            "data": [
+                {"date": "9-Apr-12", "close": 240},
+                {"date": "7-Apr-12", "close": 368},
+                {"date": "5-Apr-12", "close": 150},
+                {"date": "4-Apr-12", "close": 264},
+                {"date": "3-Apr-12", "close": 120},
+                {"date": "2-Apr-12", "close": 250}
+            ]
+        },
+        {
+            "name": "Products sell",
+            "color": "#FF8E64",
+            "value": "9,483",
+            "diff": "-9%",
+            "data": [
+                {"date": "9-Apr-12", "close": 436},
+                {"date": "7-Apr-12", "close": 221},
+                {"date": "5-Apr-12", "close": 313},
+                {"date": "4-Apr-12", "close": 264},
+                {"date": "3-Apr-12", "close": 229},
+                {"date": "2-Apr-12", "close": 218}
+            ]
+        },
+        {
+            "name": "Unic users",
+            "color": "#6CDFFF",
+            "value": "109,330",
+            "diff": "-1%",
+            "data": [
+                {"date": "9-Apr-12", "close": 336},
+                {"date": "7-Apr-12", "close": 260},
+                {"date": "5-Apr-12", "close": 120},
+                {"date": "4-Apr-12", "close": 300},
+                {"date": "3-Apr-12", "close": 250},
+                {"date": "2-Apr-12", "close": 155}
+            ]
+        },
+        {
+            "name": "Customers",
+            "color": "#FFE164",
+            "value": "477",
+            "diff": "+2",
+            "data": [
+                {"date": "9-Apr-12", "close": 136},
+                {"date": "7-Apr-12", "close": 321},
+                {"date": "5-Apr-12", "close": 213},
+                {"date": "4-Apr-12", "close": 364},
+                {"date": "3-Apr-12", "close": 129},
+                {"date": "2-Apr-12", "close": 418}
+            ]
+        }
+    ];
+
     init_donut_chart($('.donutChart_1'));
+
+    $('.areaChartFamily_1').each(function (ind) {
+        init_area_family_chart($(this), big_chart);
+    });
 
     $('.areaChart_1').each(function (ind) {
         init_area_chart($(this));
@@ -106,7 +197,7 @@ function init_line_area2_chart(el) {
 
     gradient.append("svg:stop")
         .attr("offset", "100%")
-        .attr("stop-color", "#fff")
+        .attr("stop-color", "#f6f6f6")
         .attr("stop-opacity", 0);
 
     chart.append('svg:path')
@@ -155,12 +246,11 @@ function init_line_area_chart(el) {
 
     var valueline = d3.svg.line()
         .x(function (d) {
-            console.log(d);
+            //console.log(d);
             return x(d.date);
         })
         .y(function (d) {
-            console.log(d);
-
+            //console.log(d);
             return y(d.close);
         });
     //.interpolate("cardinal");
@@ -219,7 +309,7 @@ function init_line_area_chart(el) {
 
     gradient.append("svg:stop")
         .attr("offset", "100%")
-        .attr("stop-color", "#fff")
+        .attr("stop-color", "#f6f6f6")
         .attr("stop-opacity", 0);
 
     svg.append("path")
@@ -253,7 +343,7 @@ function init_line_area_chart(el) {
         .attr("cy", function (d) {
             return y(d.close);
         });
-    
+
 }
 
 function init_line_chart(el) {
@@ -298,7 +388,6 @@ function init_line_chart(el) {
     data.forEach(function (d) {
         d.date = parseDate(d.date);
         d.close = +d.close;
-        d.open = +d.open;
     });
 
 // Scale the range of the data
@@ -315,6 +404,82 @@ function init_line_chart(el) {
         .attr("id", "blueLine")
         .attr("d", valueline(data));
 
+}
+
+function init_area_family_chart(el, data_files, data_colors) {
+
+    el.empty();
+
+    var legendBlock = el.parents('.graph-unit').find('.legend_v2');
+
+    if (!legendBlock.length) {
+        legendBlock = $('<ul class="legend_v2 graph-unit-legend" />');
+        el.parents('.graph-unit').append(legendBlock);
+    }
+
+    legendBlock.empty();
+
+    var margin = {top: 0, right: 0, bottom: 0, left: 0},
+        width = el.width() - margin.left - margin.right,
+        height = el.height() - margin.top - margin.bottom;
+
+    var parseDate = d3.time.format("%d-%b-%y").parse;
+
+    var area_x = d3.time.scale()
+        .range([0, width]);
+
+    var area_y = d3.scale.linear()
+        .range([height, 0]);
+
+    var area = d3.svg.area()
+        .x(function (d) {
+            return area_x(d.date);
+        })
+        .y0(height)
+        .y1(function (d) {
+            return area_y(d.close);
+        });
+
+
+    var svg = d3.select(el[0]).append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    for (var i = 0; i < data_files.length; i++) {
+        var data = data_files[i].data;
+
+        data.forEach(function (d) {
+            d.date = parseDate(d.date);
+            d.close = +d.close;
+        });
+
+        area_x.domain(d3.extent(data, function (d) {
+            return d.date;
+        }));
+        area_y.domain([0, d3.max(data, function (d) {
+            return d.close;
+        })]);
+
+        svg.append("path")
+            .datum(data)
+            .attr("class", "area area_v1")
+            .attr("d", area)
+            .style("fill", function (d) {
+
+                var color = data_files[i].color,
+                    legendItem = $('<li class="legend_item" />')
+                        .append($('<div class="legend_name" />').css('color', color).append($('<span/>').text(data_files[i].name)))
+                        .append($('<div class="legend_val" />')
+                            .append($('<span class="val" />').text(data_files[i].value))
+                            .append($('<sup class="graph-dynamica" />').addClass(/-/g.test(data_files[i].diff) ? 'dynamica_down' : 'dynamica_up').text(data_files[i].diff)));
+
+                legendBlock.append(legendItem);
+                return color;
+            })
+            .style("opacity", .6);
+    }
 
 }
 
@@ -382,7 +547,7 @@ function init_area_chart(el) {
 
         gradient.append("svg:stop")
             .attr("offset", "100%")
-            .attr("stop-color", "#fff")
+            .attr("stop-color", "#f6f6f6")
             .attr("stop-opacity", 0);
 
         svg.append("path")
@@ -403,7 +568,7 @@ function init_donut_chart(el) {
 
     if (!legendBlock.length) {
         legendBlock = $('<ul class="legend_v1" />');
-        el.after($('<ul class="legend_v1" />'));
+        el.after(legendBlock);
     }
 
     legendBlock.empty();
