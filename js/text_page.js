03dd3bdeb0cee78403418e9e80a2e850d542c27c
naverstay <3a4f044785G->
2016-06-14 09:51:48 +0300

    auth validation

--- js/text_page.js
+++ js/text_page.js
@@ -19,5 +19,27 @@ $(function ($) {
             $(this).closest('.filter-holder').addClass('current').siblings().removeClass('current');
         });
 
-
+    $('.validateMe').validationEngine({
+        //binded                   : false,
+        scroll: false,
+        showPrompts: true,
+        showArrow: false,
+        inlineValidation: true,
+        addSuccessCssClassToField: 'success',
+        addFailureCssClassToField: 'error',
+        parentFieldClass: '.formCell',
+        parentFormClass: '.formHolder',
+        promptPosition: "centerRight",
+        //doNotShowAllErrosOnSubmit: true,
+        //focusFirstField          : false,
+        autoHidePrompt: true,
+        autoHideDelay: 2000,
+        autoPositionUpdate: true,
+        //prettySelect             : true,
+        //useSuffix                : "_VE_field",
+        addPromptClass: 'relative_mode only_1_msg',
+        showOneMessage: false
+    });
+    
 });
+
